b71f8ae630512d71e4d0866b2bc8daab05888577
hzxfelps <felipe.augusto.gonzales1a@gmail.com>
2025-08-27 08:20:30 -0300

    20250827
    Example exercise about modificators

---
+++ exampleModificators/ObjetoEstados.java
@@ -0,0 +1,10 @@
+package exampleModificators;
+
+public class ObjetoEstados {
+    public static void main (String[]args){
+
+        for(Estados estados: Estados.values()){
+            System.out.println(estados);
+        }
+    }
+}
